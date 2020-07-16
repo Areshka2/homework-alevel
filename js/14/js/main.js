703cdbc987a4b2f7aeff2dd5b6f7c197a2de44a3
Areshka <oleg1990moskalenko@gmail.com>
2020-07-16 23:58:55 +0300

    added task 1-4

--- js/14/js/main.js
+++ js/14/js/main.js
@@ -13,63 +13,60 @@ const pizzaListBlock = document.querySelector('.pizza-list');
 function cardTemplate(pizza) {
   const imgPath = 'img';
 
-  return `
-    <div class="pizza-item col mb-5">
-      <div class="card h-100 pb-3 pt-2">
-        <img class="card-img-top w-50 mx-auto" src="${imgPath}/${pizza.img}">
-        <p class="pizza-caloricity">${pizza.caloricity || ''} кал</p>
-        <div class="card-body">
-          <h5 class="card-title">${pizza.name || ''}</h5>
-          <p class="card-text">${pizza.composition.join(', ') || ''}</p>
-        </div>
-        <div class="card-footer">
-          <p class="card-price">${pizza.price || ''} ₴</p>
-        </div>
-        <a href="#" class="btn btn-add-to-basket d-block">+</a>
-      </div>
+  const pizzaItem = document.createElement('div');
+  pizzaItem.className = "pizza-item col mb-5";
+
+  const card = document.createElement('div');
+  card.className = "card h-100 pb-3 pt-2";
+
+  // 4. Добавить возможность добовлять пиццу в избранное
+  //! add to favorite
+  const btnFav = document.createElement('button');
+  btnFav.classList.add('btn-favorite');
+  btnFav.addEventListener('click', function () {
+    this.classList.toggle('active');
+    if (this.classList.contains('active')) {
+      pizza.isFavorite = true;
+    } else {
+      pizza.isFavorite = false;
+    }
+  })
+
+  card.innerHTML = `    
+    <img class="card-img-top w-50 mx-auto" src="${imgPath}/${pizza.img}">
+    <p class="pizza-caloricity">${pizza.caloricity || ''} кал</p>    
+    <div class="card-body">
+      <h5 class="card-title">${pizza.name || ''}</h5>
+      <p class="card-text">${pizza.composition.join(', ') || ''}</p>
     </div>
+    <div class="card-footer">
+      <p class="card-price">${pizza.price || ''} ₴</p>
+    </div>
+    <a href="#" class="btn btn-add-to-basket d-block">+</a>      
   `;
+  card.appendChild(btnFav);
+  pizzaItem.appendChild(card);
+  return pizzaItem;
 }
 
 //! render pizzas function
 function renderPizzas(pizzaArr) {
-  let fragment = '';
+  pizzaListBlock.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
 
   pizzaArr.forEach((pizza => {
     const pizzaItem = cardTemplate(pizza);
-    fragment += pizzaItem;
+    fragment.appendChild(pizzaItem);
   }));
 
-  pizzaListBlock.innerHTML = '';
-  pizzaListBlock.insertAdjacentHTML('afterbegin', fragment);
+  pizzaListBlock.appendChild(fragment);
 }
 
 // loaded Pizzas
 document.addEventListener('DOMContentLoaded', function () {
   renderPizzas(pizzas);
 });
-
-
-//! Search Pizzas Function
-// get search form
-const searchForm = document.querySelector('.form-search');
-
-const searchPizzas = () => {
-  // get input for search
-  const searchInputValue = searchForm.querySelector('input[type="search"]').value;
-
-  const result = pizzas.filter(pizza => {
-    const namesSearch = pizza.name.toLocaleLowerCase().includes(searchInputValue.toLocaleLowerCase());
-    const compositionsSearch = pizza.composition.join(', ').toLocaleLowerCase().includes(searchInputValue.toLocaleLowerCase());
-
-    return namesSearch || compositionsSearch;
-  })
-
-  renderPizzas(result);
-}
-
-searchForm.oninput = searchPizzas;
-
 
 //! sort Pizzas Function
 const sortPizzas = (array, order = "default") => {
@@ -89,3 +86,73 @@ sortSelect.onchange = e => {
   pizzaListBlock.innerHTML = '';
   renderPizzas(sortPizzas(pizzas, sortSelect.value));
 }
+
+//1. Добавить возможность поика по состовляющим пиццы (compositions)
+//! Search Pizzas Function
+const searchForm = document.querySelector('.form-search');
+
+const searchPizzas = () => {
+  // get input for search
+  const searchInputValue = searchForm.querySelector('input[type="search"]').value;
+
+  const result = pizzas.filter(pizza => {
+    const namesSearch = pizza.name.toLocaleLowerCase().includes(searchInputValue.toLocaleLowerCase());
+    const compositionsSearch = pizza.composition.join(', ').toLocaleLowerCase().includes(searchInputValue.toLocaleLowerCase());
+
+    return namesSearch || compositionsSearch;
+  })
+
+  renderPizzas(result);
+}
+
+searchForm.oninput = searchPizzas;
+
+// 2. Добавить фильтрацию по-калориям и по-цене
+//! filter Pizzas by Price Function
+const filterByPrice = (e) => {
+  e.preventDefault();
+
+  let min = pizzas.reduce((acc, curr) => acc.price < curr.price ? acc : curr);
+  let max = pizzas.reduce((acc, curr) => acc.price > curr.price ? acc : curr);
+
+  minPrice.placeholder = min.price;
+  maxPrice.placeholder = max.price;
+
+  const minPriceValue = minPrice.value ? minPrice.value : min.price;
+  const maxPriceValue = maxPrice.value ? maxPrice.value : max.price;
+
+  const result = pizzas.filter(item => item.price >= minPriceValue && item.price <= maxPriceValue)
+  renderPizzas(result);
+}
+
+const filterPrice = document.querySelector('.filter-price')
+const btnFilterByPrice = filterPrice.querySelector('.btn');
+btnFilterByPrice.addEventListener('click', filterByPrice);
+
+//! filter Pizzas by Caloricity Function
+const filterByCalorisity = (e) => {
+  e.preventDefault();
+
+  let min = pizzas.reduce((acc, curr) => acc.caloricity < curr.caloricity ? acc : curr);
+  let max = pizzas.reduce((acc, curr) => acc.caloricity > curr.caloricity ? acc : curr);
+
+  minCaloricity.placeholder = min.caloricity;
+  maxCaloricity.placeholder = max.caloricity;
+
+  const minCaloricityValue = minCaloricity.value ? minCaloricity.value : min.caloricity;
+  const maxCaloricityValue = maxCaloricity.value ? maxCaloricity.value : max.caloricity;
+
+  const result = pizzas.filter(item => item.caloricity >= minCaloricityValue && item.caloricity <= maxCaloricityValue)
+  renderPizzas(result);
+}
+
+const filterCaloricity = document.querySelector('.filter-caloricity');
+const btnFilterByCaloricity = filterCaloricity.querySelector('.btn');
+btnFilterByCaloricity.addEventListener('click', filterByCalorisity);
+
+//3. Добавить кнопку сброса фильтров.
+document.querySelector('.btn-reset').addEventListener('click', function (e) {
+  e.preventDefault();
+  this.parentNode.reset();
+  renderPizzas(pizzas);
+})
